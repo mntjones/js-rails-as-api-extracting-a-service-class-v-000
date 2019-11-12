446ee35eb5784041165f7e50b1649f452b2c7e79
Monica Tamaru Jones <mntamaru@gmail.com>
2019-11-12 23:22:56 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,4 +1,9 @@
 class SightingsController < ApplicationController
+    
+    def index
+      sightings = Sighting.all
+      render json: SightingSerializer.new(sightings).to_serialized_json
+    end
     def show
         @sighting = Sighting.find(params[:id])
         render json: SightingSerializer.new(sightings).to_serialized_json
